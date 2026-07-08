8fe3ee4ded6901188d1fb6a81cfde6c8bf719189
Arseniy Klempner <arseniyk@status.im>
2026-07-08 11:00:44 -0600

    phase1-fix: move RLN FFI exports from liblogosdelivery/declare_lib.nim into library/declare_lib.nim
    Upstream reshaped the library dir; our carry declare_lib.nim at the OLD path
    (liblogosdelivery/declare_lib.nim) is no longer imported by anyone, so
    logosdelivery_{set_rln_fetcher,set_rln_config,set_rln_identity,push_roots,push_proof}
    were being dropped from the dylib as dead code.
    
    Fix: append the 5 RLN FFI procs (with LogosDelivery type) to the upstream
    declare_lib.nim that library/liblogosdelivery.nim actually imports.
    
    Discovered while building phase2 delivery-module plugin: plugin cpp/h ref
    these functions, but liblogosdelivery.dylib exported only 14 stock symbols.

--- library/declare_lib.nim
+++ library/declare_lib.nim
@@ -1,6 +1,7 @@
 import ffi
 import std/locks
 import logos_delivery
+import logos_delivery/waku/waku_mix/logos_core_client as mix_rln_client
 
 declareLibrary("logosdelivery")
 
@@ -31,3 +32,40 @@ proc logosdelivery_set_event_callback(
 
   ctx[].eventCallback = cast[pointer](callback)
   ctx[].eventUserData = userData
+
+proc logosdelivery_set_rln_fetcher(
+    ctx: ptr FFIContext[LogosDelivery], fetcher: mix_rln_client.RlnFetcherFunc, fetcherData: pointer
+) {.dynlib, exportc, cdecl.} =
+  if fetcher.isNil:
+    echo "error: nil fetcher in logosdelivery_set_rln_fetcher"
+    return
+  mix_rln_client.setRlnFetcher(fetcher, fetcherData)
+
+proc logosdelivery_set_rln_config(
+    ctx: ptr FFIContext[LogosDelivery], configAccountId: cstring, leafIndex: cint
+): cint {.dynlib, exportc, cdecl.} =
+  if configAccountId.isNil:
+    return RET_ERR
+  mix_rln_client.setRlnConfig($configAccountId, leafIndex.int)
+  return RET_OK
+
+proc logosdelivery_set_rln_identity(
+    ctx: ptr FFIContext[LogosDelivery], idSecretHashHex: cstring
+) {.dynlib, exportc, cdecl.} =
+  if idSecretHashHex.isNil:
+    return
+  mix_rln_client.setRlnIdentity($idSecretHashHex)
+
+proc logosdelivery_push_roots(
+    ctx: ptr FFIContext[LogosDelivery], rootsJson: cstring
+) {.dynlib, exportc, cdecl.} =
+  if rootsJson.isNil:
+    return
+  mix_rln_client.pushRoots($rootsJson)
+
+proc logosdelivery_push_proof(
+    ctx: ptr FFIContext[LogosDelivery], proofJson: cstring
+) {.dynlib, exportc, cdecl.} =
+  if proofJson.isNil:
+    return
+  mix_rln_client.pushProof($proofJson)
